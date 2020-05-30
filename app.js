3b24a7648ed9e8b908a8a9bf7a8b2ee7a35705ab
Sarah Rainsberger <sarahrainsberger@gmail.com>
2020-05-30 14:14:45 +0000

    Got navigation active links working

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 import RouterHandler from './router.js'
 
-// window.onhashchange = () => {
-//     setActiveLink ();
-// }
 
-// function setActiveLink(){
-//     const links = documents.querySelectorAll('.header-link');
-//     links.forEach(link => {
-//         const linkPath = link.getAttribute('href');
-//         const currentPath=window.location.hash;
-//         if(currentPath === linkPath){
-//             link.classLink.add('active');
-//         } else {
-//             link.classLink.remove('active');
-//         }
-//     })
-// }
+
+window.onhashchange = () => {
+  setActiveLink();
+}
+
+function setActiveLink() {
+   const links = document.querySelectorAll('.header-link');
+   links.forEach(link => {
+      const linkPath = link.getAttribute('href');
+      const currentPath = window.location.hash;
+      if (currentPath === linkPath) {
+        link.classList.add('active');  
+      } else {
+        link.classList.remove('active');         
+      }
+   });
+}
 
 class App {
     constructor () {
